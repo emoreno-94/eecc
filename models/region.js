d10164e9c4018299979e611b56d5f9abe2e90697
Edward <edward.moreno@ing.uchile.cl>
2020-07-31 10:50:05 -0400

    Remove seeds and include "Ñuble"
    Se crean migraciones de los datos iniciales y se agrega la "nueva" región Ñuble

--- models/region.js
+++ models/region.js
@@ -2,33 +2,10 @@ const rfr = require('rfr');
 const knex = rfr('/lib/db/knex');
 const atie = rfr('/lib/db/addTransactionIfExists');
 
-
 const tableName = 'species_region';
 
-const regionsId = {
-  'Arica y Parinacota': 1,
-  'Tarapacá': 2,
-  'Antofagasta': 3,
-  'Atacama': 4,
-  'Coquimbo': 5,
-  'Valparaíso continental': 6,
-  'Metropolitana': 7,
-  'O\'higgins': 8,
-  'Maule': 9,
-  'Bío-Bío': 10,
-  'Araucanía': 11,
-  'De Los Ríos': 12,
-  'De Los Lagos': 13,
-  'Aysén': 14,
-  'Magallanes continental e insular': 15,
-  'Antártica': 16,
-  'Isla Pascua': 17,
-  'Salas y Gómez': 18,
-  'Juan Fernandez': 19,
-  'Desventuradas': 20,
-};
 
-const getInstance = ({ regionName, value, speciesHash }) => ({ region_id: regionsId[regionName], species_hash: speciesHash, value });
+const getInstance = ({ regionName, value, speciesHash }) => ({ region_name: regionName, species_hash: speciesHash, value });
 
 const insert = (validCategory, { transaction } = {}) => atie(knex(tableName).insert(validCategory).returning('id'), transaction);
 
--- models/validCategory.js
+++ models/validCategory.js
@@ -3,26 +3,10 @@ const rfr = require('rfr');
 const knex = rfr('/lib/db/knex');
 const atie = rfr('/lib/db/addTransactionIfExists');
 
-
 const tableName = 'species_valid_category';
 
-const categoriesId = {
-  CR: 1,
-  DD: 2,
-  EN: 3,
-  EW: 4,
-  EX: 5,
-  FP: 6,
-  IC: 7,
-  LC: 8,
-  NT: 9,
-  R: 10,
-  VU: 11,
-  'Taxón no presente en Chile': 12,
-  otro: 13,
-};
 
-const getInstance = ({ shortName, speciesHash }) => ({ valid_category_id: categoriesId[shortName], species_hash: speciesHash });
+const getInstance = ({ shortName, speciesHash }) => ({ valid_category_short_name: shortName, species_hash: speciesHash });
 
 const findBy = (filter = {}, { transaction } = {}) => atie(knex(tableName).select().where(filter), transaction);
 
@@ -31,7 +15,7 @@ const findFirstBy = (filter = {}, options) => findBy(filter, options).first();
 const insert = (validCategory, { transaction } = {}) => atie(knex(tableName).insert(validCategory).returning('id'), transaction);
 
 const tryToInsert = async (validCategory, { transaction } = {}) => {
-  validCategory.valid_category_id = validCategory.valid_category_id || categoriesId.otro; // clasificar como otro por defecto
+  validCategory.valid_category_short_name = validCategory.valid_category_short_name || 'otro'; // clasificar como "otro" por defecto
 
   const dbSpecies = await findFirstBy(validCategory, { transaction });
   if (!dbSpecies) {
